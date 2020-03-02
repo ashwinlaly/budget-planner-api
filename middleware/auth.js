9d1da5ea7a16304e7e29b9ad056c227be476ed8e
Ashwin <ashwinlaly@gmail.com>
2020-03-02 17:37:49 +0530

    Middleware changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,7 @@ let jwt = require('jsonwebtoken'),
 
 module.exports = function(){
     return  function(req, res, next) {
+        console.log(auth)
         jwt.verify(req.headers['auth'], process.env.JWT_TOKEN, (err, decode) => {
             if(err) {
                 responseHelper.sendError(res, err)
